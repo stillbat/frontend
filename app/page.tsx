5a1575d0ae0439daa998d94c2599ebb101a6ec34
Bat Byambasuren <batb@Bats-Mac-mini.local>
2024-04-27 11:04:54 -0500

    neg hiigedeh

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,64 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+"use client";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    fetch("http://localhost:1000/stocks", {
+      headers: { Authorization: `Bearer ${localStorage.getItem("token")}` },
+    }).then(async (res) => {
+      if (res.status == 401) {
+        router.push("/login");
+      }
+      if (res.ok) {
+        const data = await res.json();
+        setData(data);
+      }
+    });
+    if (localStorage.getItem("token") == undefined) {
+      router.push("/login");
+    } else {
+    }
+  }, []);
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div
+      style={{
+        display: "flex",
+        gap: "20px",
+        flexWrap: "wrap",
+      }}
+    >
+      {data.map(
+        (item: {
+          datetime: string;
+          open: string;
+          high: string;
+          low: string;
+          close: string;
+          volume: string;
+          
+        }) => (
+          <div
+            style={{
+              backgroundColor: "white",
+              height: "200px",
+              width: "300px",
+            }}
           >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+            <p>datetime:{item.datetime}</p>
+            <p>date: {item.datetime.split(" ")[0]}</p>
+            <p>time: {item.datetime.split(" ")[1]}</p>
+            <p>open:{item.open}</p>
+            <p>high:{item.high}</p>
+            <p>low:{item.low}</p>
+            <p>close:{item.close}</p>
+            <p>volume: {item.volume}</p>
+          </div>
+        )
+      )}
+    </div>
   );
 }
---
+++ app/register/page.tsx
@@ -0,0 +1,109 @@
+"use client";
+
+import { useRouter } from 'next/navigation';
+import { ChangeEventHandler, useState } from "react";
+
+export default function Register() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const router = useRouter();
+    const handleEmailChange: ChangeEventHandler<HTMLInputElement> = (e) => { setEmail(e.target.value) };
+    const handlePasswordChange: ChangeEventHandler<HTMLInputElement> = (e) => { setPassword(e.target.value) };
+    const handleSignUp = async () => {
+        try {
+            const res = await fetch("http://localhost:1000/register", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ email, password }),
+            })
+            if(res.status == 200) {
+                const data = await res.json();
+                router.push("/login");
+            } else if (res.status == 401) {
+            throw new Error("Unouthorized");
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    return (
+        <div style={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "100vh",
+        }}
+        >
+            <div style={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                padding: "32px 32px 32px 32px",
+                backgroundColor: "rgba(235, 235, 235, 1)",
+                width: "280px",
+                height: "350px",
+                borderRadius: "16px"
+            }}>
+                <div style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "left",
+                    alignItems: "left",
+                    padding: "32px 32px 32px 32px",
+                    backgroundColor: "white",
+                    borderRadius: "16px",
+                    gap: "20px"
+                }}>
+                    <div style={{
+                        display: "flex",
+                        flexDirection: "column",
+                    }}>
+                        <label style={{ opacity: "50%", fontSize: "10px" }}>Email</label>
+                        <input type="email" placeholder="" onChange={handleEmailChange}
+                            style={{
+                                padding: "15px",
+                                margin: "10px, 0px",
+                                borderRadius: "2px",
+                                border: "1px solid rgba(102, 102, 102, 0.35)",
+                                width: "180px"
+                            }}
+                        />
+                    </div>
+                    <div style={{
+                        display: "flex",
+                        flexDirection: "column",
+                    }}>
+                        <label style={{ opacity: "50%", fontSize: "10px" }}>Password</label>
+                        <input type="password" placeholder="" onChange={handlePasswordChange}
+                            style={{
+                                padding: "15px",
+                                margin: "10px, 0px",
+                                borderRadius: "2px",
+                                border: "1px solid rgba(102, 102, 102, 0.35)",
+                                width: "180px"
+                            }}
+                        />
+                    </div>
+                    <div style={{
+                        display: "flex",
+                        flexDirection: "column",
+                    }}>
+                        <button onClick={handleSignUp}
+                            style={{
+                                width: "100%",
+                                padding: "15px",
+                                margin: "10px, 0px",
+                                borderRadius: "2px",
+                                border: "1px solid rgba(102, 102, 102, 0.35)"
+                            }}
+                        >Register</button>
+                    </div>
+                </div>
+            </div >
+        </div>
+    );
+};
