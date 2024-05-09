f0406b86e6efa477b1ebd3a06703bf55a12c6695
Bat Byambasuren <batb@Bats-Mac-mini.local>
2024-05-09 10:12:37 -0500

    psuh

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,133 +1,157 @@
 "use client";
 import React, { ChangeEventHandler, useState } from "react";
-import { useRouter } from 'next/navigation'
-import { link } from "fs";
-
-
-
-
-
-
-
+import { useRouter } from "next/navigation";
+import Image from "next/image";
 
 export default function Login() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const router = useRouter()
-    const handleEmailChange: ChangeEventHandler<HTMLInputElement> = (e) => { setEmail(e.target.value) };
-    const handlePasswordChange: ChangeEventHandler<HTMLInputElement> = (e) => { setPassword(e.target.value) };
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
+  const handleEmailChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setEmail(e.target.value);
+  };
+  const handlePasswordChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    setPassword(e.target.value);
+  };
 
-    const handleLogin = async () => {
-        try {
-            const res = await fetch("https://backend-gej8.onrender.com/login", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                },
-                body: JSON.stringify({ email, password }),
-            })
-            if(res.status == 200) {
-                const data = await res.json();
-                localStorage.setItem("token", data)
-                router.push("/");
-            } else if (res.status == 401) {
-            throw new Error("Unouthorized");
-            }
-        } catch (error) {
-            console.log(error);
-        }
-    };
-    return (
-        <div style={{
+  const handleLogin = async () => {
+    try {
+      const res = await fetch("https://backend-gej8.onrender.com/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+      if (res.status == 200) {
+        const data = await res.json();
+        localStorage.setItem("token", data);
+        router.push("/");
+      } else if (res.status == 401) {
+        throw new Error("Unouthorized");
+      }
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  return (
+    
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          height: "100vh",
+          background: "url(/z4.png)",        
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "contain",
+      }}
+      >
+        <div
+          style={{
             display: "flex",
             flexDirection: "column",
             justifyContent: "center",
             alignItems: "center",
-            height: "100vh",
-        }}
+            padding: "32px 32px 32px 32px",
+            backgroundColor: "rgba(235, 235, 235, 1)",
+            width: "280px",
+            height: "350px",
+            borderRadius: "16px",
+          }}
         >
-            <div style={{
+          <div
+            style={{
+              display: "flex",
+              flexDirection: "column",
+              justifyContent: "left",
+              alignItems: "left",
+              padding: "32px 32px 32px 32px",
+              backgroundColor: "white",
+              borderRadius: "16px",
+              gap: "20px",
+            }}
+          >
+            <div
+              style={{
                 display: "flex",
                 flexDirection: "column",
-                justifyContent: "center",
-                alignItems: "center",
-                padding: "32px 32px 32px 32px",
-                backgroundColor: "rgba(235, 235, 235, 1)",
-                width: "280px",
-                height: "350px",
-                borderRadius: "16px"
-            }}>
-                <div style={{
-                    display: "flex",
-                    flexDirection: "column",
-                    justifyContent: "left",
-                    alignItems: "left",
-                    padding: "32px 32px 32px 32px",
-                    backgroundColor: "white",
-                    borderRadius: "16px",
-                    gap: "20px"
-                }}>
-                    <div style={{
-                        display: "flex",
-                        flexDirection: "column",
-                    }}>
-                        <label style={{ opacity: "50%", fontSize: "10px" }}>Email</label>
-                        <input type="email" placeholder="" onChange={handleEmailChange}
-                            style={{
-                                padding: "15px",
-                                margin: "10px, 0px",
-                                borderRadius: "2px",
-                                border: "1px solid rgba(102, 102, 102, 0.35)",
-                                width: "180px"
-                            }}
-                        />
-                    </div>
-                    <div style={{
-                        display: "flex",
-                        flexDirection: "column",
-                    }}>
-                        <label style={{ opacity: "50%", fontSize: "10px" }}>Password</label>
-                        <div style={{
-                            display: "flex",
-                            alignItems: "center",
-                        }}>
-                            <input type="password" placeholder="" onChange={handlePasswordChange}
-                                style={{
-                                    padding: "15px",
-                                    margin: "10px, 0px",
-                                    borderRadius: "2px",
-                                    border: "1px solid rgba(102, 102, 102, 0.35)",
-                                    width: "180px",
-                                }}
-                            />
-                            {/* <div style={{ display: "flex,", position: "absolute", right: "1180px" }}>
-                                <img src="./eye-open3.png" width={25} height={25} />
-                                <img src="./eye-close3.png" width={25} height={25} />
-                            </div> */}
-                        </div>
-                    </div>
-                    <div style={{
-                        display: "flex",
-                        flexDirection: "column",
-                    }}>
-                        <button onClick={handleLogin}
-                            style={{
-                                width: "100%",
-                                padding: "15px",
-                                margin: "10px, 0px",
-                                borderRadius: "2px",
-                                border: "1px solid rgba(102, 102, 102, 0.35)"
-                            }}
-                        >Login</button>
-                    </div>
-                    <div style={{
-                        display: "flex",
-                        flexDirection: "column",
-                    }}>
-                        <a href="/register">register</a>
-                    </div>
-                </div>
-            </div >
+              }}
+            >
+              <label style={{ opacity: "50%", fontSize: "10px" }}>Email</label>
+              <input
+                type="email"
+                placeholder=""
+                onChange={handleEmailChange}
+                style={{
+                  padding: "15px",
+                  margin: "10px, 0px",
+                  borderRadius: "2px",
+                  border: "1px solid rgba(102, 102, 102, 0.35)",
+                  width: "180px",
+                }}
+              />
+            </div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <label style={{ opacity: "50%", fontSize: "10px" }}>
+                Password
+              </label>
+              <div
+                style={{
+                  display: "flex",
+                  alignItems: "center",
+                }}
+              >
+                <input
+                  type="password"
+                  placeholder=""
+                  onChange={handlePasswordChange}
+                  style={{
+                    padding: "15px",
+                    margin: "10px, 0px",
+                    borderRadius: "2px",
+                    border: "1px solid rgba(102, 102, 102, 0.35)",
+                    width: "180px",
+                  }}
+                />
+              </div>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <button
+                onClick={handleLogin}
+                style={{
+                  width: "100%",
+                  padding: "15px",
+                  margin: "10px, 0px",
+                  borderRadius: "2px",
+                  border: "1px solid rgba(102, 102, 102, 0.35)",
+                }}
+              >
+                Login
+              </button>
+            </div>
+            <div
+              style={{
+                display: "flex",
+                flexDirection: "column",
+              }}
+            >
+              <a href="/register">register</a>
+            </div>
+          </div>
         </div>
-    );
-};
+      </div>
+    
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,44 @@
 "use client";
 import { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
+import * as React from "react";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+
+interface TabPanelProps {
+  children?: React.ReactNode;
+  index: number;
+  value: number;
+}
+
+function CustomTabPanel(props: TabPanelProps) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index: number) {
+  return {
+    id: `simple-tab-${index}`,
+    "aria-controls": `simple-tabpanel-${index}`,
+  };
+}
 
 export default function Home() {
   const router = useRouter();
@@ -23,41 +61,146 @@ export default function Home() {
     }
   }, []);
 
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event: React.SyntheticEvent, newValue: number) => {
+    setValue(newValue);
+  };
+  const [ticker, setTicker] = useState();
+  function tickerhandle(event: any) {
+    setTicker(event.target.value);
+  }
+  const [decimal, setDecimal] = useState();
+  function decimalhandle(event: any) {
+    setDecimal(event.target.value);
+  }
+  const [outputsize, setOutputsize] = useState();
+  function outputhandle(event: any) {
+    setOutputsize(event.target.value);
+  }
+  const [interval, setInterval] = useState();
+  function intervalhandle(event: any) {
+    setInterval(event.target.value);
+  }
+  const [startdate, setDate] = useState();
+  function startdatehandle(event: any) {
+    setDate(event.target.value);
+  }
+  const [starttime, setStarttime] = useState();
+  function starttimehandle(event: any) {
+    setStarttime(event.target.value);
+  }
+
+
+
+
   return (
-    <div
-      style={{
-        display: "flex",
-        gap: "20px",
-        flexWrap: "wrap",
-      }}
-    >
-      {data.map(
-        (item: {
-          datetime: string;
-          open: string;
-          high: string;
-          low: string;
-          close: string;
-          volume: string;
-        }) => (
+    <Box sx={{ width: "100%" }}>
+      <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+        <Tabs
+          value={value}
+          onChange={handleChange}
+          aria-label="basic tabs example"
+        >
+          <Tab label="Data" {...a11yProps(0)} />
+          <Tab label="Create API" {...a11yProps(1)} />
+          <Tab label="Whatever goes here" {...a11yProps(2)} />
+        </Tabs>
+      </Box>
+      <CustomTabPanel value={value} index={0}>
+        <div>
           <div
-            key={item.datetime}
             style={{
-              backgroundColor: "white",
-              height: "200px",
-              width: "300px",
+              display: "flex",
+              gap: "20px",
+              flexWrap: "wrap",
             }}
           >
-            <p>date: {item.datetime.split(" ")[0]}</p>
-            <p>time: {item.datetime.split(" ")[1]}</p>
-            <p>open:{item.open}</p>
-            <p>high:{item.high}</p>
-            <p>low:{item.low}</p>
-            <p>close:{item.close}</p>
-            <p>volume: {item.volume}</p>
+            {data.map(
+              (item: {
+                datetime: string;
+                open: string;
+                high: string;
+                low: string;
+                close: string;
+                volume: string;
+              }) => (
+                <div
+                  key={item.datetime}
+                  style={{
+                    backgroundColor: "white",
+                    height: "200px",
+                    width: "180px",
+                  }}
+                >
+                  <p>date: {item.datetime.split(" ")[0]}</p>
+                  <p>time: {item.datetime.split(" ")[1]}</p>
+                  <p>open:{item.open}</p>
+                  <p>high:{item.high}</p>
+                  <p>low:{item.low}</p>
+                  <p>close:{item.close}</p>
+                  <p>volume: {item.volume}</p>
+                </div>
+              )
+            )}
           </div>
-        )
-      )}
-    </div>
+        </div>
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={1}>
+        <div
+          style={{
+            display: "flex",
+            gap: "20px",
+            flexWrap: "wrap",
+          }}
+        >
+          <p>
+            "https://api.twelvedata.com/time_series?apikey=110574cb274041938b5bde5630eeb858&interval=
+            {interval}min&symbol=
+            {ticker}&dp={decimal}
+            &outputsize={outputsize}
+            &type=stock&dp=2&timezone=exchange&start_date={startdate}
+            {" "}{starttime}&end_date=2024-04-17 15:59:00&format=JSON"
+          </p>
+          <p>390 length 1min chart</p>
+          <input
+            placeholder="stock name"
+            value={ticker}
+            onChange={tickerhandle}
+          ></input>
+          <input
+            placeholder="decimal places"
+            value={decimal}
+            onChange={decimalhandle}
+          ></input>
+          <input
+            placeholder="output size"
+            value={outputsize}
+            onChange={outputhandle}
+          ></input>
+          <input
+            placeholder="interval"
+            value={interval}
+            onChange={intervalhandle}
+          ></input>
+          <input
+            type="date"
+            placeholder="startdate"
+            value={startdate}
+            onChange={startdatehandle}
+          ></input>
+          <input
+            type="time" step="1"
+            placeholder="starttime"
+            value={starttime}
+            onChange={starttimehandle}
+          ></input>
+          
+        </div>
+      </CustomTabPanel>
+      <CustomTabPanel value={value} index={2}>
+        Item Three
+      </CustomTabPanel>
+    </Box>
   );
 }
